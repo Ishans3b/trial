a1eaa817815788a9bdcb53e40698733d66465b61
Ishan8827 <ishan.patel@s3bglobal.com>
2022-05-30 09:44:57 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useReducer } from "react";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        react trial
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+const[nam,setnaam]=useState({
+    name:"",
+    password:""
+});
+
+let name,value
+const handleinputs=(e)=>{
+e.preventDefault();
+name=e.target.name;
+value=e.target.value;
+
+setnaam({...nam,[name]:value})
+}
+
+console.log(nam)
+
+  
+
+
+    return(
+        <div>
+            
+                <label>
+                    Name
+                </label>
+                <input type="text" name="name"  onChange={handleinputs}></input>
+            <label>
+                    Password
+                </label>
+                <input type="password" name="password"  onChange={handleinputs}></input>
+            {/* <button onClick={()=>{
+                axios({
+                    method: 'post',
+                    url: '/user/12345',
+                    data: {
+                      firstName: 'Fred',
+                      lastName: 'Flintstone'
+                    }
+                  });
+            }}></button> */}
+               
+                
+        </div>
+    )
 }
 
 export default App;
