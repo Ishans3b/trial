d08d9ba092935b015171809b8bfb098908d4b3d7
Ishans3b <104881360+Ishans3b@users.noreply.github.com>
2022-06-01 15:32:07 +0530

    app commited

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect } from "react";
 import { useReducer } from "react";
 import { useState } from "react";
@@ -28,7 +27,7 @@ console.log(nam)
         <div>
             
                 <label>
-                    Name
+                    Namesdsddsdsdsdsd
                 </label>
                 <input type="text" name="name"  onChange={handleinputs}></input>
             <label>
@@ -49,30 +48,6 @@ console.log(nam)
                 
         </div>
     )
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        react trial dfdfdf
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> c2acb4d6c2ca160efa1cf7776398a14b5b4b695b
 }
 
 export default App;
